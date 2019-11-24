b96cf43e5c91a1eb827cd75dc8a1c0aab9f68b00
SueTingYeh <rockkrisklose@gmail.com>
2019-11-24 22:30:19 +0800

    ver 2

---
+++ arrow-funtion.js
@@ -0,0 +1,19 @@
+// if you only have one statement in  the function
+
+const add = (a,b) =>{
+    return a + b ;
+}
+
+//you can short it for like this
+const add = (a,b) => a + b;
+
+// if your function has only one argument, you can wirte it like this
+//Remove the parentheses
+// const addOne = (a) => a+1;
+const addOne = a => a+1;
+
+
+// if your function has no arguments, you need to keep the parentheses
+const noArgument = () => 8+9;
+
+
---
+++ first-app.js
@@ -0,0 +1,27 @@
+const name ='YEH';
+let age =27;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = (userName,userAge,userHasHobby) => {
+    
+    return ('Name is '+ userName +',age is '
+    + userAge +
+    ', and ' +
+    userHasHobby
+    ); 
+}
+
+
+// if you only have one statement in  the function
+/*
+const add = (a,b) =>{
+    return a + b ;
+}
+*/
+//you can short it for like this
+const add = (a,b) => a + b;
+
+console.log(add(1,2));
+console.log(summarizeUser(name,age,hasHobbies));
